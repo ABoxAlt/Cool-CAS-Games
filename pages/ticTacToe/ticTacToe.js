43848731c3cb5a2aeb91081c97a0b7a891fcee00
ABoxAlt <1121133@apps.nsd.org>
2025-07-21 14:40:21 -0700

    Updated Tic Tac Toe

--- pages/ticTacToe/ticTacToe.js
+++ pages/ticTacToe/ticTacToe.js
@@ -20,7 +20,7 @@ function onMouseUp(e) {
 function drawBoard() {
     const hudColor = 'rgba(82, 90, 167, 1)';
     const backColor = 'rgba(174, 182, 255, 1)';
-    const lineColor = 'rgba(136, 148, 255, 1)';
+    const squareColor = 'rgba(136, 148, 255, 1)';
     //This sets the background
     //ctx.save saves any changes made to css so it can be restored after these actions
     ctx.save();
@@ -32,22 +32,13 @@ function drawBoard() {
     ctx.fillStyle = hudColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height/4);
     ctx.fillRect(0, 0, canvas.width/4, canvas.height);
-    ctx.strokeStyle = lineColor;
-    //This sets the linewidth
-    //canvas being divided and using Math.floor() just gives 6 in this case but if this were to
-    //be scaled up it would still be the same dimensions
-    ctx.lineWidth = Math.floor(canvas.width/36);
-    ctx.beginPath();
-    for (let i = 200; i < 400; i += 100) {
-        ctx.moveTo(i, 100);
-        ctx.lineTo(i, 400);
-    }
-    for (let i = 200; i < 400; i += 100) {
-        ctx.moveTo(100, i);
-        ctx.lineTo(400, i);
-    }
-    ctx.stroke();
-    
+    ctx.fillStyle = squareColor;
+    ctx.fillRect(100, 100, canvas.width/4, canvas.height/4);
+    ctx.fillRect(100, 300, canvas.width/4, canvas.height/4);
+    ctx.fillRect(300, 100, canvas.width/4, canvas.height/4);
+    ctx.fillRect(300, 300, canvas.width/4, canvas.height/4);
+    ctx.fillRect(200, 200, canvas.width/4, canvas.height/4);
+
     ctx.restore();
 }
 
