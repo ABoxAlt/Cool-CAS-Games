958a68d435d51d85bbffc42e565e4b9b1df43e4f
ABoxAlt <1121133@apps.nsd.org>
2025-07-22 17:05:07 -0700

    Tic Tac Toe Update
    Made symbols fit within squares
    
    Not much left for this one

--- pages/ticTacToe/ticTacToe.js
+++ pages/ticTacToe/ticTacToe.js
@@ -9,12 +9,58 @@ const ctx = canvas.getContext('2d');
 //they're usually bad since they stop everything to run the event
 canvas.addEventListener("mouseup", onMouseUp);
 
-const symbolSize = 75;
+const symbolSize = (canvas.width/4) * 0.5;
+
+let turn = 0;
+let win = 3;
+const board = [[3, 3, 3], [3, 3, 3], [3, 3, 3]];
 
 //Outputs the mouse coordinates (based on the canvas not the webpage)
 function onMouseUp(e) {
-    drawX(e.offsetX, e.offsetY);
-    drawO(e.offsetX, e.offsetY);
+    if (e.offsetX < 100 || e.offsetY < 100) {
+        console.log("out of bounds")
+        return;
+    }
+    let x = parseInt(e.offsetX);
+    let y = parseInt(e.offsetY);
+
+    const xOffset = 25;
+    const yOffset = 25;
+
+    if (x < 2 * (canvas.width/4)) {
+        x = canvas.width/4;
+    } else if (x < 3 * (canvas.width/4)) {
+        x = 2 * (canvas.width/4);
+    } else if (x < canvas.width) {
+        x = 3 * (canvas.width/4);
+    } else {
+        console.log('Error, could not place symbol');
+    }
+
+    if (y < 2 * (canvas.height/4)) {
+        y = canvas.height/4;
+    } else if (y < 3 * (canvas.height/4)) {
+        y = 2 * (canvas.height/4);
+    } else if (y < canvas.height) {
+        y = 3 * (canvas.height/4);
+    } else {
+        console.log('Error, could not place symbol');
+    }
+
+    if (board[((y/100) - 1)][((x/100) - 1)] != 3) {
+        console.log("symbol allready placed in square");
+        return;
+    }
+
+    if (turn == 0) {
+        drawX(x + xOffset, y + yOffset);
+        turn ++;
+        board[((y/100) - 1)][((x/100) - 1)] = 0;
+    } else {
+        drawO(x + xOffset, y + yOffset);
+        turn --;
+        board[((y/100) - 1)][((x/100) - 1)] = 1;
+    }
 }
 
 function drawBoard() {
@@ -68,9 +114,10 @@ function drawO(x, y) {
     ctx.restore();
 }
 
+
 function gameStart() {
     drawBoard();
-
+    turn = 0;
 }
 
 gameStart();
