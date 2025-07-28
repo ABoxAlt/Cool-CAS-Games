ec8e193ff1775f3a0fd3a82f4f44c723135e935b
ABoxAlt <1121133@apps.nsd.org>
2025-07-27 20:57:33 -0700

    Added dinner fact

--- pages/coolDinnerFacts/coolDinnerFacts.js
+++ pages/coolDinnerFacts/coolDinnerFacts.js
@@ -1,7 +1,8 @@
 const facts = [
     "51 is divisible by 17 and 3.",
     "In 1492, Columbus sailed the ocean blue and discovered the Americas.",
-    "The Doppler Effect describes the change in frequency of a wave when the observer or source or both of a wave are moving."
+    "The Doppler Effect describes the change in frequency of a wave when the observer or source or both of a wave are moving.",
+    "Cheetas can run 60 mph."
 ]
 
 function getRandomInt(max) {
