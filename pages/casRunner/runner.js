4560b21502365f1dcc11b4f02e2ddcb3781847cb
ABoxAlt <1121133@apps.nsd.org>
2025-08-14 14:50:03 -0700

    More work on the runner game

--- pages/casRunner/runner.js
+++ pages/casRunner/runner.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector(".gameCanvas");
 const ctx = canvas.getContext('2d');
 
-objColor = "rgb(48, 57, 138)";
+//objColor = "rgb(48, 57, 138)";
 floorLevel = 300;
 
 
@@ -11,13 +11,24 @@ class obstacle {
         this.width = width;
         this.x = canvas.width;
         this.y = floorLevel;
+        this.speed = 5;
+        this.active = true;
+        this.objColor = "rgba(160, 170, 255, 1)";
     }
 
-    drawRectangle() {
+    drawObstacle() {
         ctx.save();
-        ctx.fillStyle = objColor;
+        ctx.fillStyle = this.objColor;
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.restore();
+    }
+
+    moveObstacle() {
+        if (this.x > 0 - this.width) {
+            this.x -= this.speed;
+        } else if (this.x <= 0 - this.width) {
+            this.active = false;
+        }
     }
 }
 
@@ -25,23 +36,24 @@ class playerClass {
     constructor() {
         this.width = 50;
         this.height = 50;
-        this.x = 0;
+        this.x = 100;
         this.y = floorLevel;
         this.jump = 0;
         this.maxJumpSpeed = 12
         this.jumpSpeed = this.maxJumpSpeed;
+        this.playerColor = "rgb(48, 57, 138)";
     }
     
     drawPlayer() {
         ctx.save();
-        ctx.fillStyle = objColor;
+        ctx.fillStyle = this.playerColor;
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.restore();
     }
 
     checkJump() {
         if (this.jump == 1) {
-            console.log("jumping " + this.jump);
+            //console.log("jumping " + this.jump);
             this.y -= this.jumpSpeed;
             this.jumpSpeed -= .5;
             if (this.y <= 0 || this.jumpSpeed <= 0) {
@@ -50,7 +62,7 @@ class playerClass {
             }
         
         } else if (this.jump == 2 && this.y < floorLevel) {
-            console.log("falling " + this.jump);
+            //console.log("falling " + this.jump);
             this.y += this.jumpSpeed;
             this.jumpSpeed += .5;
             if (this.y >= floorLevel) {
@@ -65,11 +77,20 @@ class playerClass {
 }
 
 const obstacles =[];
+
+function destroyObstacles() {
+    for (const ob of obstacles) {
+        if (!ob.active) {
+            //console.log("Destroyed obstacle with index : " + obstacles.indexOf(ob));
+            obstacles.splice(0, 1);
+        }
+    }
+}
+
 const player = new playerClass();
 
 addEventListener("keydown", keyDown);
 addEventListener("keyup", keyUp);
-
 
 function keyDown(e) {
     if (player.jump != 0) {
@@ -85,9 +106,33 @@ function keyDown(e) {
         case "ArrowUp":
             player.jump = 1;
             break;
+        case "e":
+            spawnObstacle();
+            break;
+        case "f":
+            console.log(obstacles);
+            break;
         default:
             break;
     }
+}
+
+function spawnObstacle() {
+    const obstacleType = random(3);
+    switch (obstacleType) {
+        case 0:
+            obstacles.push(new obstacle(30));
+            break;
+        case 1:
+            obstacles.push(new obstacle(50));
+            break;
+        case 2:
+            obstacles.push(new obstacle(70));
+            break;
+        default:
+            console.log("There was an error when spawning an object. obstacleType was : " + obstacleType);
+    }
+    console.log("Debug: Spawned Obstacle");
 }
 
 function keyUp(e) {
@@ -127,13 +172,18 @@ function random(max) {
 
 
 
-
+function checkObstacles() {
+    destroyObstacles();
+    for (const ob of obstacles) {
+        ob.moveObstacle();
+    }
+}
 
 function drawBackdrop() {
     ctx.save();
     ctx.fillStyle = " rgb(255, 222, 130)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = objColor;
+    ctx.strokeStyle = "rgb(48, 57, 138)";
     ctx.lineWidth = 4;
     ctx.beginPath();
     ctx.moveTo(0, 352);
@@ -152,7 +202,7 @@ async function waitFor(milliseconds) {
 function paint() {
     drawBackdrop();
     for (const ob of obstacles) {
-        ob.drawRectangle();
+        ob.drawObstacle();
     }
     player.drawPlayer();
 }
@@ -161,6 +211,8 @@ async function gameLoop() {
     while (true) {
         await waitFor(10);
         player.checkJump();
+        checkObstacles();
+
         paint();
     }
 }
