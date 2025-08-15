d8b1441dbdef44897d08e81e353926efc9e54405
Bill Song <voidcaster.michaelwang@gmail.com>
2025-08-15 16:58:01 -0700

    work on chess

--- pages/chess/chess.js
+++ pages/chess/chess.js
@@ -2,74 +2,113 @@
 const canvas = document.querySelector(".gameCanvas");
 
 //This lets us draw things on the canvas
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 //This adds an event for when the mouse is pressed down, events aren't great for a lot of games
 //but they work fine for tic tac toe since you are just selecting options
 //they're usually bad since they stop everything to run the event
 canvas.addEventListener("mouseup", onMouseUp);
 
-let turn = 0;
-let win = false;
+const GameState = Object.freeze({
+    SELECT: 0,
+    MOVE: 1,
+    PROMOTE: 2,
+    END: 3
+});
+
+let turn;
 let board;
-const allPieces = [];
+const ALL_PIECES = [];
+let state = GameState.SELECT;
+let selectedPiece = null;
+const CHESS_IMAGES = loadImages();
 
 //Outputs the mouse coordinates (based on the canvas not the webpage)
 function onMouseUp(e) {
-    if (win) {
-        win = false;
-        gameStart();
-        return;
+    switch (state) {
+        case GameState.SELECT: {
+            const [x, y] = getMousePos(e);
+            selectedPiece = board[y][x];
+            if (selectedPiece === null)
+                return;
+            else {
+                state = GameState.MOVE;
+                ctx.save();
+                ctx.strokeColor = "white";
+                ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+                ctx.restore();
+            }
+            break;
+        }
+        case GameState.MOVE: {
+            const [x, y] = getMousePos(e);
+            let response = selectedPiece.checkMove(x, y);
+
+            // check if piece was clicked again, indicating a cancelled move
+            if (selectedPiece.x == x && selectedPiece.y == y) {
+                // TODO: erase black outline around piece
+                drawBoard();
+                state = GameState.SELECT;
+            }
+            if (!response.isValid)
+                return;
+
+            // check if there are squares that need to be empty
+            if (Object.hasOwn(response, "squares")) {
+                let invalid = false;
+                for (const s of response.squares) {
+                    let square = board[s[1]][s[0]];
+                    if (square !== null) {
+                        invalid = true;
+                        break;
+                    }
+                }
+                if (!invalid) {
+                    // TODO:
+                    state = GameState.SELECT;
+                }
+            }
+            // TODO:
+            turn++;
+            break;
+        }
+        case GameState.PROMOTE:
+
+            break;
+        case GameState.END:
+            state = GameState.SELECT;
+            gameStart();
+            break;
+        default:
+            console.log("ERROR: state unset??");
+            break;
     }
 
+    checkWin();
+}
+
+function getMousePos(e) {
+    // get mouse position
     let x = parseInt(e.offsetX);
     let y = parseInt(e.offsetY);
 
-    const xOffset = 25;
-    const yOffset = 25;
+    console.log(`X: ${x}, Y: ${y}`);
 
-    if (x < 100) {
-        x = 0;
-    } else if (x < 200) {
-        x = 100;
-    } else if (x < 300) {
-        x = 200;
-    } else {
-        console.log('Error, could not place symbol');
-    }
+    // scale to board
+    x = Math.floor( x / 50 );
+    y = Math.floor( y / 50 );
 
-    if (y < 100) {
-        y = 0;
-    } else if (y < 200) {
-        y = 100;
-    } else if (y < 300) {
-        y = 200;
-    } else {
-        console.log('Error, could not place symbol');
-    }
+    return [ x, y ];
+}
 
-    if (board[((y/100))][((x/100))] != 3) {
-        console.log("symbol allready placed in square");
-        return;
-    }
-
-    if (turn == 0) {
-        drawX(x + xOffset, y + yOffset);
-        turn ++;
-        board[((y/100))][((x/100))] = 0;
-    } else {
-        drawO(x + xOffset, y + yOffset);
-        turn --;
-        board[((y/100))][((x/100))] = 1;
-    }
-    checkWin();
+function checkWin() {
 }
 
 const cellSize = canvas.width / 8;
 async function drawBoard() {
-    console.log("drawBoard")
-    const lightColor = 'rgba(174, 182, 255, 1)';
-    const darkColor = 'rgba(136, 148, 255, 1)';
+    console.log("drawBoard");
+    const lightColor = "rgba(174, 182, 255, 1)";
+    const darkColor = "rgba(136, 148, 255, 1)";
     //This sets the background
     //ctx.save saves any changes made to css so it can be restored after these actions
     ctx.save();
@@ -94,21 +133,20 @@ async function drawBoard() {
     }
 
     // wait for img to load
-    let allImg = [];
-    for (piece of allPieces) {
-        allImg.push(piece.sprite);
-    }
-
     await Promise.all(
-        allImg.map(
+        Object.values(CHESS_IMAGES).map(
             (image) =>
-            new Promise((resolve) => image.addEventListener("load", resolve)),
+            new Promise((resolve) => image.addEventListener("load", resolve))
+            //     {
+            //     console.log(image);
+            //     resolve();
+            // })
         ),
     );
 
     // draw pieces
-    console.log(allPieces);
-    allPieces.forEach(element => {
+    console.log(ALL_PIECES);
+    ALL_PIECES.forEach(element => {
         console.log("foreach");
         element.draw();
     });
@@ -120,6 +158,36 @@ function gameStart() {
     board = createBoard();
     drawBoard();
     turn = 0;
+}
+
+async function loadImages() {
+    let allPiecesNames = [
+        "rook",
+        "knight",
+        "bishop",
+        "queen",
+        "king",
+        "pawn"
+    ];
+    let colors = [
+        "black",
+        "white"
+    ];
+
+    // TODO: refactor into Dictionary if it's beneficial
+    let output = {};
+    for (const n of allPiecesNames) {
+        for (const c of colors) {
+            let img = new Image();
+            let filename = `${n}_${c}.png`;
+            img.src = "../../images/chess-pieces/" + filename;
+            output[filename] = img;
+        }
+    }
+
+    console.log(output);
+
+    return output;
 }
 
 function createBoard() {
@@ -140,7 +208,7 @@ function createBoard() {
     board.push(bBack);
     
     // black pawns
-    let bPawns = []
+    let bPawns = [];
     for (let x = 0; x < 8; x++) {
         bPawns.push(new Pawn(true, x, 1));
     }
@@ -149,18 +217,20 @@ function createBoard() {
 
     // empty no mans land
     for (let y = 0; y < 4; y++) {
-        let a = []
+        let a = [];
         for (let x = 0; x < 8; x++) {
-            a.push(false);
+            a.push(null);
         }
-        board.push(a)
+        board.push(a);
     }
 
     // white pawns
-    let wPawns = []
+    let wPawns = [];
     for (let x = 0; x < 8; x++) {
         wPawns.push(new Pawn(false, x, 6));
     }
+
+    board.push(wPawns);
 
     // white back rank
     let wBack = [
@@ -178,9 +248,9 @@ function createBoard() {
 
     // add all pieces to an array
     let pieceRows = [bBack, bPawns, wBack, wPawns];
-    for (row of pieceRows) {
-        for (piece of row) {
-            allPieces.push(piece);
+    for (const row of pieceRows) {
+        for (const piece of row) {
+            ALL_PIECES.push(piece);
         }
     }
 
@@ -190,43 +260,82 @@ function createBoard() {
 window.onload = () => gameStart();
 
 class Piece {
-    sprite;
+    spriteName;
     name;
 
     constructor(isBlack, x, y, name) {
         this.name = name;
-        console.log(this.name)
+        console.log(this.name);
         this.isBlack = isBlack;
-        this.sprite = new Image();
-        this.sprite.src = `../../images/chess-pieces/${this.name}_${this.isBlack ? "black" : "white"}.png`;
+        this.spriteName = `${this.name}_${this.isBlack ? "black" : "white"}.png`;
         this.x = x;
         this.y = y;
-        console.log("constructor")
+        console.log("constructor");
     }
 
     draw() {
-        console.log("draw")
-        console.log(this.sprite);
+        console.log("draw");
         ctx.drawImage(
-            this.sprite,
+            CHESS_IMAGES[this.spriteName],
             this.x * cellSize,
             this.y * cellSize,
             cellSize,
             cellSize
         );
     }
+
+    // abstract method (override it in child classes to use)
+    checkMove(x, y) {
+        console.log(`${x}, ${y}`);
+        throw new Error("You have to implement the method checkMove!");
+    }
 }
 
 class Pawn extends Piece {
+    hasMoved = false;
+
     constructor(isBlack, x, y) {
         super(isBlack, x, y, "pawn");
     }
-    hasMoved = false;
+
+    // checkMove(x, y) {
+    //     
+    // }
 }
 
 class Rook extends Piece {
     constructor(isBlack, x, y) {
         super(isBlack, x, y, "rook");
+    }
+
+    checkMove(x, y) {
+        // check if piece is on vertical or horizontal axes
+        let dX = this.x - x;
+        let dY = this.y - y;
+
+        let xMoved = dX != 0;
+        let yMoved = dY != 0;
+        
+        // if piece hasn't been moved or has been moved on both axes then the 
+        // move is invalid
+        if (xMoved == yMoved) {
+            return {isValid: false};
+        }
+
+        // else the move is valid. find squares that have to be empty for move
+        // to be valid
+        let squares = [];
+
+        let start = xMoved ? this.x : this.y;
+        let end = xMoved ? x : y;
+        let isPositive = end - start > 0;
+        let delta = isPositive ? 1 : -1;
+
+        for (let i = start; i != end; i += delta) {
+            squares.push([xMoved ? i : x, yMoved ? y : i]);
+        }
+
+        return {isValid: true, squares: squares};
     }
 }
 
